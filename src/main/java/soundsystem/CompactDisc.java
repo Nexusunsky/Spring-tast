38196d6c8816b30eb11c840214148a4c5d5b299e
haoliu <haoliu@thoughtworks.com>
2018-10-25 09:27:42 +0800

    Clean for Demo of testy

--- src/main/java/soundsystem/CompactDisc.java
+++
@@ -1,8 +0,0 @@
-package soundsystem;
-
-/**
- * @author: haoliu on 2018/10/12 07:03
- */
-public interface CompactDisc {
-    void play();
-}
--- src/main/java/soundsystem/MediaPlayer.java
+++
@@ -1,8 +0,0 @@
-package soundsystem;
-
-/**
- * @author: haoliu on 2018/10/12 07:34
- */
-public interface MediaPlayer {
-    void play();
-}
